9c8197fa54de9c56f0e64d47078803ef05335b14
DrSans69 <145922388+DrSans69@users.noreply.github.com>
2023-12-18 12:34:06 +0200

    Update benchmark.py

--- my_package/benchmark.py
+++ my_package/benchmark.py
@@ -1,9 +1,7 @@
-from sys import stdout
 from time import perf_counter
-
-
-def benchmark(iters=1):
-    def decorator(func):
+    
+def benchmark(iter: int = 1) -> callable:
+    def decorator(func: callable) -> callable:
         def wrapper(*args, **kwargs):
             all = 0
             for i in range(iters):
@@ -11,6 +9,6 @@ def benchmark(iters=1):
                 func(*args, **kwargs)
                 stop_time = perf_counter()
                 all += stop_time - start_time
-            stdout.write(str(all / iters))
+            print(all / iters)
         return wrapper
     return decorator
